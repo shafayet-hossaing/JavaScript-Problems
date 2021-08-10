f98d5ef5fb4a366b92f80cef5b0e495eff91a903
shafayet-hossain <shafayethossaing4609@gmail.com>
2021-08-11 00:26:12 +0600

    EvenOrOdd

---
+++ EvenOrOdd.js
@@ -0,0 +1,10 @@
+
+// Even Or Odd Number
+function isEven(number) {
+    if (number % 2 == 0) { // Setting Up The Condition Whether Even Or Odd
+        return "Even Number"; // If True It Will Be Executed
+    }
+    return "odd Number"; // If False It Won’t Be Executed
+}
+let number = isEven(47) // Calling The Function With Argument In A variable
+console.log(number); // Here You Go
