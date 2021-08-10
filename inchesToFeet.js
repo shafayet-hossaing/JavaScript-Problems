6c952f0c58a57b02e55b457d26fd3f0e411b8644
shafayet-hossain <shafayethossaing4609@gmail.com>
2021-08-11 00:18:16 +0600

    InchesToFeet-Calculation

---
+++ inchesToFeet.js
@@ -0,0 +1,6 @@
+function inchesToFeet(inches) {
+    let feet = inches / 12; //  // Main Calculation == Dividing Inches By 12
+    return feet; // Return Feet Variable For Putting The Function In A New Variable
+}
+let givenInches = inchesToFeet(24) // Calling The Function With Inches
+console.log(givenInches);
