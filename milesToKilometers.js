7d68fe682ed51d916e3bbd59ef33421f80d13fdc
shafayet-hossain <shafayethossaing4609@gmail.com>
2021-08-11 00:23:14 +0600

    MilesToKilometers

---
+++ milesToKilometers.js
@@ -0,0 +1,8 @@
+
+// Miles To Kilometers
+function milesToKilometers(miles) {
+    let kilometers = miles * 1.60934; // Main Calculation == Multiplying Miles By 1.60934
+    return kilometers; // Return Kilometers Variable For Putting The Function In A New Variable
+}
+let givenMiles = milesToKilometers(50) // Calling The Function With Argument
+console.log(givenMiles);
